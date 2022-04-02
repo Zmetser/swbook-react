b6be1d15cfa473ee525b50a3ccdbaf03054b64b9
Dávid Végh <davidvegh94@gmail.com>
2022-04-02 17:21:52 +0200

    egy kis css

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -4,6 +4,18 @@ import ListItem from "./PeopleListItem"
 function List({ list }) {
 
     const [included, setIncluded] = useState('')
+    const [clicked, setClicked] = useState()
+
+    const clickHandler = (event) => {
+        setClicked(prev => {
+            if (prev === event.target.innerText) {
+                return null
+            }
+            else {
+                return event.target.innerText
+            }
+        })
+    }
 
     const changeHandler = (event) => {
         setIncluded(event.target.value)
@@ -11,16 +23,16 @@ function List({ list }) {
 
     return (
         <>
+            <form>
+                <input type='text' onChange={changeHandler} className="inp" placeholder="Search" />
+            </form>
             <table className="listtable">
                 {list.map((item, index) => {
-                    if(item.name.toLowerCase().includes(included)){
-                        return <ListItem character={item} key={index} />
+                    if (item.name.toLowerCase().includes(included)) {
+                        return <ListItem character={item} key={index} clickHandler={clickHandler} clicked={clicked === item.name} cname="active" />
                     }
                 })}
             </table>
-            <form>
-                <input type='text' onChange={changeHandler} />
-            </form>
         </>
     )
 }
--- src/components/PeopleListItem.js
+++ src/components/PeopleListItem.js
@@ -1,20 +1,32 @@
 
 
-function ListItem({ character }) {
+function ListItem({ character, clickHandler, clicked, cname }) {
 
     return (
         <>
-            <tr>
-                <td colSpan={2}><strong>{character.name}</strong></td>
+            <tr className={clicked && cname}>
+                <td colSpan={2}><a href='#' onClick={clickHandler}>{character.name}</a></td>
             </tr>
-            <tr>
-                <td>Height:</td>
-                <td>{character.height}</td>
-            </tr>
-            <tr>
-                <td>Mass:</td>
-                <td>{character.mass}</td>
-            </tr>
+            {clicked && (
+                <>
+                    <tr className={clicked && cname}>
+                        <td>Height:</td>
+                        <td>{character.height}</td>
+                    </tr>
+                    <tr className={clicked && cname}>
+                        <td>Mass:</td>
+                        <td>{character.mass}</td>
+                    </tr>
+                    <tr className={clicked && cname}>
+                        <td>Gender:</td>
+                        <td>{character.gender}</td>
+                    </tr>
+                    <tr className={clicked && cname}>
+                        <td>Skin color:</td>
+                        <td>{character.skin_color}</td>
+                    </tr>
+                </>
+            )}
         </>
     )
 }
